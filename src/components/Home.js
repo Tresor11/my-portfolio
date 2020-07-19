19788dbcb975086edf6a6f2fffd8b27594ad1ee8
Tresor11 <tresor.moise2001@gmail.com>
2020-07-19 17:22:53 +0200

    added the basic layout

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Home=()=>{
+    return(
+        <div className="home">
+            <div className="intro-cont">
+                <div className="picture">
+                </div>
+                <div className="description">
+                    Hello <span role="img">👋</span>, <br/>I'm Tresor a Full stack developer based in Rwanda
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
