daf602e919c1fe87a69b6f63e909156cc6f5d8d1
Tresor11 <tresor.moise2001@gmail.com>
2020-07-23 13:13:04 +0200

    added the home page animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TypeIt from "typeit-react";
+import Particles from 'react-particles-js';
+import * as params from './particle.json';
 
 const Home = () => {
   return (
@@ -31,6 +33,21 @@ const Home = () => {
             <button className="action-btn button">View my work</button>
           </a>
         </div>
+      </div>
+
+      {/* .ripple-background
+  .circle.xxlarge.shade1
+  .circle.xlarge.shade2
+  .circle.large.shade3
+  .circle.mediun.shade4
+  .circle.small.shade5 */}
+
+      <div className="ripple-background">
+        <div  className=" circle xxlarge shade1"></div>
+        <div  className=" circle xlarge shade2"></div>
+        <div className=" circle large shade3"></div>
+        <div className=" circle medium shade4"></div>
+        <div className=" circle small shade5"></div>
       </div>
     </div>
   );
