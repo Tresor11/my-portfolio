5bb361001c9744c0241d10557583a32c7fbe6583
Tresor11 <bireketresor@gmail.com>
2024-06-08 19:15:30 +0200

    remove project section import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles/App.scss';
 import Home from './components/Home';
 import About from './components/About';
-import Project from './components/Project';
 import Articles from './components/Articles';
 import Contact from './components/Contact';
 
